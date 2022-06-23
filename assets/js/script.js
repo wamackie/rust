2059d7420c9b08601bc67ae604163b3d3282ad4e
William <wamackie8456@gmail.com>
2022-06-22 20:36:22 -0400

    new post wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,9 @@ function greet(name) {
     console.log('Hello ' + name);
 }
 
+function newThread() {
+    document.getElementById("new-thread").innerHTML = "Hello World";
+}
+
 greet('John');
 greet('Mary');
